9d6aae74e6a43d641e99f003fca99e04066b52e7
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-29 23:49:00 +0530

    Added BST

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+
+public class Driver {
+	public static void main(String args[]) {
+		Tree<Integer> tree = new Tree<>();
+		tree.addNode(56);
+		tree.addNode(30);
+		tree.addNode(70);
+		tree.preOrder(tree.root);
+	}
+	
+}
---
+++ src/Tree.java
@@ -0,0 +1,49 @@
+
+public class Tree<T extends Comparable<T>> {
+	Node<T> root;
+	Tree(){
+		root = null;
+	}
+	void addNode(T data) {
+		Node<T> node = new Node<>(data);
+		if(root == null) {
+			root = node;
+		}
+		else {
+			Node<T> iterator = root;
+			while(iterator != null) {
+				if((iterator.data.compareTo(data)) > 0) {
+					//left branch
+					if(iterator.left == null) {
+						iterator.left = node;
+						return;
+					}
+					else {
+						iterator = iterator.left;
+					}
+				}
+				else {
+					//right branch
+					if(iterator.right == null) {
+						iterator.right = node;
+						return;
+					}
+					else {
+						iterator = iterator.right;
+					}
+				}
+			}
+		}
+	}
+	
+	public void preOrder(Node<T> node) {
+		if(node == null) {
+			return;
+		}
+		else {
+			preOrder(node.left);
+			System.out.println(node.data);
+			preOrder(node.right);
+		}
+	}
+}
