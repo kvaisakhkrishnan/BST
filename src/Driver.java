3e28f591213f2730fab194c34a08d67f5b1ddb55
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-30 08:26:48 +0530

    Large Tree Created

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,17 @@ public class Driver {
 		tree.addNode(56);
 		tree.addNode(30);
 		tree.addNode(70);
+		tree.addNode(22);
+		tree.addNode(40);
+		tree.addNode(11);
+		tree.addNode(3);
+		tree.addNode(16);
+		tree.addNode(70);
+		tree.addNode(60);
+		tree.addNode(95);
+		tree.addNode(65);
+		tree.addNode(63);
+		tree.addNode(67);
 		tree.preOrder(tree.root);
 		tree.search(tree.root, 30);
 	}
